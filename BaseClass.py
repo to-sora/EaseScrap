874d1162859247431931a4277137eb227815515e
waitor <waitotsang@outlook.com>
2023-10-04 15:52:15 +0800

    first commit

---
+++ BaseClass.py
@@ -0,0 +1,558 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import re
+import pickle
+import csv
+import _thread
+import re
+import random
+import os
+import time
+import hashlib
+from urllib.parse import urljoin
+import csv
+import threading
+import queue
+import tqdm
+import time
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+import re
+import hashlib
+import os
+from bs4 import BeautifulSoup
+
+
+def setup():
+    '''
+    return selenium fire fox headless
+    
+    '''
+
+    options = Options()
+    options.headless = True
+
+    
+    #options.binary_location = 'geckodriver'
+    driver = webdriver.Firefox(options=options)
+    return driver
+
+def extract_chinese(inputstring):
+
+    '''
+    extract chinese word from html and keep emojis and punctuation marks
+
+    '''
+    inputstring = str(inputstring)
+    chinese_pattern = re.compile(r'[\u4e00-\u9fff]+|[^\u4e00-\u9fff\s]+')
+    chinese_words = chinese_pattern.findall(inputstring)
+    print(chinese_words)
+    chinese_words = "".join(chinese_words)
+    chinese_words = chinese_words.replace('\n', ' ')
+    
+    return chinese_words
+
+from abc import ABC, abstractmethod
+class PageInterface(ABC):
+    """
+    An abstract base class for defining the interface of a web page.
+
+    Attributes:
+    -----------
+    None
+
+    Methods:
+    --------
+    get_title() -> str:
+        Returns the title of the web page.
+
+    save_html(dir: str):
+        Saves the HTML content of the web page to a specified directory.
+
+    extract_text(dir: str):
+        Extracts the text content of the web page and saves it to a specified directory.
+
+    get_state() -> dict:
+        Returns the state of the web page.
+
+    extract_links(dir: str = '') -> list:
+        Extracts the links from the web page and returns them as a list.
+
+    worker_func(**kwargs) -> tuple:
+        A worker function that performs a specific task on the web page and returns the result as a tuple.
+    """
+    @abstractmethod
+    def __init__(self, htm:str, url:str, tiitle='' , dir='',loger= None):
+        pass
+    @abstractmethod
+    def get_title(self) -> str:
+        pass
+
+    @abstractmethod
+    def save_html(self, dir: str):
+        pass
+
+    @abstractmethod
+    def extract_text(self, dir: str):
+        pass
+
+    @abstractmethod
+    def get_state(self) -> dict:
+        pass
+
+    @abstractmethod
+    def extract_links(self, dir: str = '') -> list:
+        pass
+
+    @abstractmethod
+    def worker_func(self, **kwargs) -> tuple:
+        """
+        Abstract method that defines the worker function for a subclass.
+        
+        Args:
+        - **kwargs: keyword arguments that can be used by the subclass to define the worker function.
+            usually, the keyword arguments are used to pass the parameters of the worker function.
+            it will get by looper and pass to worker_func
+        
+        Returns:
+        - A tuple containing the results of the worker function.
+        """
+        pass
+    
+
+
+
+class WebsitePage(PageInterface):
+    """
+    A class representing a website page.
+
+    Attributes:
+    -----------
+    html : str
+        The HTML content of the page.
+    url : str
+        The URL of the page.
+    title : str
+        The title of the page.
+    soup : BeautifulSoup object
+        A BeautifulSoup object representing the parsed HTML content.
+    text : str
+        The extracted text content of the page.
+    extracted_pth : str
+        The path where the extracted text content is saved.
+    orginal_path : str
+        The path where the original HTML content is saved.
+    extracted : bool
+        A flag indicating whether the text content has been extracted.
+    dir : str
+        The directory where the HTML and text content will be saved.
+    uniqued_id : str
+        A unique ID generated from the title of the page.
+
+    Methods:
+    --------
+    get_title() -> str:
+        Returns the title of the page.
+    save_html(dir: str):
+        Saves the HTML content of the page to a file.
+    extract_text(dir: str):
+        Extracts the text content of the page and saves it to a file.
+    get_state() -> dict:
+        Returns a dictionary containing the state of the object.
+    """
+    def __init__(self, htm, url, tiitle='', dir='',loggingfile= None):
+        """
+        Initializes a new instance of the WebsitePage class.
+
+        Parameters:
+        -----------
+        htm : str
+            The HTML content of the page.
+        url : str
+            The URL of the page.
+        tiitle : str, optional
+            The title of the page. If not provided, it will be extracted from the HTML content.
+        dir : str, optional
+            The directory where the HTML and text content will be saved.
+        """
+        self.html = htm
+        self.url = url
+        self.soup = BeautifulSoup(self.html, 'html.parser')
+        self.text = ''
+        self.extracted_pth = ''
+        self.orginal_path = ''
+        self.extracted = False
+        self.dir = dir
+        self.loggingfile = loggingfile
+        self.islogging = True if loggingfile != '' else False
+        if tiitle == '':
+            ## extract title from html bt beautifulsoup
+            self.title = self.soup.title.string
+            ## remove invalid char for file name in Ubuntu and Windows
+            self.title = re.sub(r'[\\/:*?"<>|]', '', self.title)
+        else:
+            self.title = tiitle
+
+        ## init a unique id for titelname using 20 digits hash
+        
+        self.uniqued_id = str(hashlib.sha1(self.title.encode('utf-8')).hexdigest()[:20])
+    def log_control(self,x,*args,**kwargs):
+    ## fromat string linke print 
+        x = str(x)
+        x += ' '.join([str(i) for i in args]).replace('\n','')
+        if 'end' in kwargs:
+            x += kwargs['end']
+        
+        ## add time string
+        x = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + '  ' + x
+        if self.islogging:
+            with open(self.loggingfile,'a') as f:
+                f.write(x+'\n')
+        else:
+            print(x)
+    def get_title(self) -> str:
+        """
+        Returns the title of the page.
+
+        Returns:
+        --------
+        str
+            The title of the page.
+        """
+        return self.title
+
+    def save_html(self, dir: str):
+        """
+        Saves the HTML content of the page to a file.
+
+        Parameters:
+        -----------
+        dir : str
+            The directory where the HTML content will be saved.
+        """
+        try:
+            self.orginal_path = os.path.join(dir, str(self.title+'__'+self.uniqued_id) + '.html').strip().replace(' ', '')
+            with open(self.orginal_path, 'w') as f:
+                f.write(self.html)
+        except Exception as e:
+            self.log_control(e)
+            self.log_control('fail to save html in base class: WebsitePage')
+
+    def extract_text(self, dir: str):
+        """
+        Extracts the text content of the page and saves it to a file.
+
+        Parameters:
+        -----------
+        dir : str
+            The directory where the text content will be saved.
+        """
+        try:
+            self.text = self.soup.get_text()
+            self.extracted_pth = os.path.join(dir, str(self.title+'__'+self.uniqued_id) + '.txt').strip().replace(' ', '')
+            with open(self.extracted_pth, 'w') as f:
+                f.write(self.text)
+            self.extracted = True
+        except Exception as e:
+            self.log_control(e)
+            self.log_control('fail to extract text from base class: WebsitePage')
+
+    def get_state(self) -> dict:
+        """
+        Returns a dictionary containing the state of the object.
+
+        Returns:
+        --------
+        dict
+            A dictionary containing the state of the object.
+        """
+        return {
+            "url": self.url,
+            "title": self.title,
+            "extracted": self.extracted,
+            "extracted_pth": self.extracted_pth,
+            "orginal_path": self.orginal_path,
+            "uniqued_id": self.uniqued_id
+        }
+    
+class Controller:
+    """
+    A class that controls the web scraping process.
+
+    Attributes:
+    - init_url (str): The initial URL to start scraping from.
+    - dir (str): The directory to save the scraped data.
+    - thread_limit (int): The maximum number of threads to use for scraping.
+    - link_csv (str): The path to the CSV file containing the scraped links.
+    - driver (WebDriver): The Selenium WebDriver to use for scraping.
+    - stop (threading.Event): An event to signal when to stop scraping.
+    - page_class (WebsitePage): The class to use for parsing web pages.
+
+    Methods:
+    - __init__(self, init_url, dir, thread_limit=5, driver=None, page_class=WebsitePage):
+        Initializes the Controller object with the given parameters.
+    - get_unvisited(self, item=10, randomize=False):
+        Returns a list of unvisited URLs.
+    - save(self):
+        Saves the Controller object to a file.
+    - worker(self, input_queue, output_queue, wdata={}):
+        The worker function for multithreading.
+    - update_linkcsv(self, output_queue):
+        The thread worker for updating the link CSV file.
+    - analy_dir(self):
+        Analyzes the total size and length of text files in the directory.
+    - loop(self, no_target=2, batch=10, randomize=False, data={}):
+        The main loop for the Controller object.
+
+    """
+    def log_control(self,x,*args,**kwargs):
+        ## fromat string linke print 
+        x = str(x)
+        x += ' '.join([str(i) for i in args]).replace('\n','')
+        if 'end' in kwargs:
+            x += kwargs['end']
+        
+        ## add time string
+        x = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + '  ' + x
+        if self.islogging:
+            with open(self.loggingfile,'a') as f:
+                f.write(x+'\n')
+        else:
+            print(x)
+    def __init__(self, init_url, dir, thread_limit=5, driver=None, page_class=WebsitePage,loggingfile = ''):
+        """
+        Initializes the Controller object with the given parameters.
+
+        Parameters:
+        - init_url (str): The initial URL to start scraping from.
+        - dir (str): The directory to save the scraped data.
+        - thread_limit (int): The maximum number of threads to use for scraping.
+        - driver (WebDriver): The Selenium WebDriver to use for scraping.
+        - page_class (WebsitePage): The class to use for parsing web pages.
+        """
+        self.init_url = init_url
+        self.dir = dir
+        self.thread_limit = thread_limit
+        self.link_csv = os.path.join(dir, 'link.csv')
+        self.driver = driver
+        self.stop  = threading.Event()
+        self.page_class = page_class
+        self.loggingfile = loggingfile
+        self.islogging = True if loggingfile != '' else False
+        ###
+        if not os.path.isfile(self.link_csv):
+            with open(self.link_csv, 'w') as f:
+                log_control('create link.csv as cannot find it!')
+                f.write('url,title,extracted,extracted_pth,orginal_path,uniqued_id\n')
+                ### visit init url
+                driver.get(self.init_url)
+                driver.implicitly_wait(0.5)
+                html = driver.page_source
+                
+                novel_page = self.page_class(html, self.init_url,loggingfile=self.loggingfile)
+                novel_page.save_html(self.dir)
+                link_t_be_added = novel_page.extract_links()
+                #print(link_t_be_added)
+                for link in link_t_be_added:
+                    f.write(link+', ,F, , , \n')
+    import random
+
+    def get_unvisited(self, item=10, randomize=False):
+        '''
+        Returns a list of unvisited URLs.
+
+        Parameters:
+        - item (int): The maximum number of URLs to return.
+        - randomize (bool): Whether to return URLs in random order.
+
+        Returns:
+        - unvisited (list): A list of unvisited URLs.
+        '''
+        try:
+            unvisited = []
+            with open(self.link_csv, 'r') as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    unvisited.append(dict(row))
+                unvisited = [row['url'] for row in unvisited if row['extracted'] == 'F' or row['extracted'] == 'False' ]
+            ## random sample
+            #self.log_control('unvisited len: ', len(unvisited), 'item: ', unvisited)
+            if randomize and len(unvisited) > item:
+                unvisited = random.sample(unvisited, item)
+            else:
+                unvisited = unvisited[:item]
+        except Exception as e:
+            self.log_control(e)
+            self.log_control('fail to get unvisited url sleep 3 sec')
+            unvisited = []
+            time.sleep(3)
+        return unvisited
+
+    def save(self):
+        '''
+        Saves the Controller object to a file.
+        '''
+        savedict = {
+            'init_url': self.init_url,
+            'dir': self.dir,
+            'thread_limit': self.thread_limit,
+            'link_csv': self.link_csv
+        }
+        with open(os.path.join(self.dir, 'controller.pkl'), 'wb') as f:
+            pickle.dump(savedict, f)
+
+    def worker(self, input_queue, output_queue, wdata={}):
+        '''
+        The worker function for multithreading.
+
+        Parameters:
+        - input_queue (queue.Queue): The input queue for the worker.
+        - output_queue (queue.Queue): The output queue for the worker.
+        - wdata (dict): The data to pass to the worker function.
+        '''
+        self.log_control(wdata)
+        while not self.stop.is_set():
+            try:
+                if not self.stop.is_set():
+                    sub_url, html = input_queue.get(timeout=5)
+                else:
+                    break             
+                thispage = self.page_class(html, sub_url, dir=self.dir,loggingfile=self.loggingfile)
+                link , state = thispage.worker_func(**wdata)
+                output_queue.put((link, state))
+                self.log_control('extracted: ', state["title"])
+                input_queue.task_done()
+            except queue.Empty:
+                continue
+            except Exception as e:
+                self.log_control(e)
+                self.log_control('fail to extract text')
+                #input_queue.task_done()
+                time.sleep(1)
+        self.log_control('worker thread stop')
+
+    def update_linkcsv(self,output_queue):
+        '''
+        The thread worker for updating the link CSV file.
+
+        Parameters:
+        - output_queue (queue.Queue): The output queue for the worker.
+        '''
+        while not self.stop.is_set():
+            try:
+                if not self.stop.is_set():
+                    link, state = output_queue.get(timeout=5)
+                else:
+                    break
+                
+                with open(self.link_csv, 'r') as f:
+                    reader = csv.DictReader(f)
+                    rows = [row for row in reader]
+                for i in range(len(rows)):
+                    if rows[i]['url'] == state['url']:
+                        rows[i] = state
+                        break
+                ### insert unseen link to csv
+                for i in link:
+                    if i not in [row['url'] for row in rows]:
+                        rows.append({'url': i, 'title': '', 'extracted': 'F', 'extracted_pth': '', 'orginal_path': '', 'uniqued_id': ''})
+                with open(self.link_csv, 'w') as f:
+                    writer = csv.DictWriter(f, fieldnames=reader.fieldnames)
+                    writer.writeheader()
+                    writer.writerows(rows)
+                output_queue.task_done()
+            except queue.Empty:
+                continue
+            except Exception as e:
+                self.log_control(e)
+                self.log_control('fail to update link.csv in worker2')
+        self.log_control('update_linkcsv thread stop')
+
+    def analy_dir(self):
+        '''
+        Analyzes the total size and length of text files in the directory.
+
+        Returns:
+        - total_size (int): The total size of the directory in bytes.
+        - total_len (int): The total length of text files in the directory.
+        '''
+        total_size = 0
+        for dirpath, dirnames, filenames in os.walk(self.dir):
+            for f in filenames:
+                fp = os.path.join(dirpath, f)
+                total_size += os.path.getsize(fp)
+        # analy total len of *.txt
+        total_len = 0
+        for dirpath, dirnames, filenames in os.walk(self.dir):
+            for f in filenames:
+                if f.endswith('.txt'):
+                    fp = os.path.join(dirpath, f)
+                    with open(fp, 'r') as f:
+                        total_len += len(f.read())
+        return total_size, total_len
+
+    def loop(self, no_target=2, batch=10, randomize=False, data={},wait_time=0,use_driver=True):
+        '''
+        The main loop for the Controller object.
+
+        Parameters:
+        - no_target (int): The number of targets to scrape.
+        - batch (int): The number of URLs to scrape in each batch.
+        - randomize (bool): Whether to scrape URLs in random order.
+        - data (dict): The data to pass to the worker function.
+        '''
+        try:
+
+            input_queue = queue.Queue()
+            output_queue = queue.Queue()
+            # strart worker
+            thread_list = []
+            for i in range(self.thread_limit):
+                thread_list.append(threading.Thread(target=self.worker, args=(input_queue, output_queue), kwargs={"wdata": data}))
+
+            
+            # start update link.csv thread
+            
+            thread_list.append(threading.Thread(target=self.update_linkcsv, args=(output_queue,)))
+            for i in thread_list:
+                i.start()
+            for i in range(no_target):
+                unvisited = self.get_unvisited(batch, randomize)
+                for url in unvisited:
+                    try:
+                        if use_driver:
+
+                            self.log_control('get url: ', url)
+                            self.driver.get(url)
+                            self.log_control('geted url: ', url)
+
+
+                            # add to input queue
+                            html = self.driver.page_source
+                        else: 
+                            ## use requests
+                            self.log_control('get url: ', url)
+                            html = requests.get(url).text
+                            time.sleep(wait_time)
+                        input_queue.put((url, html))
+                        if wait_time == 0:
+                            time.sleep(random.random())
+                        else:
+                            time.sleep(wait_time)
+                    except Exception as e:
+                        self.log_control(e)
+                        self.log_control('fail to get url: ', url)
+                        continue
+            # close input queue if no more task
+            input_queue.join()
+            output_queue.join()
+            
+            self.log_control('input_queue join')
+            self.log_control('output_queue join')
+            self.stop.set()
+        except Exception as e:
+            self.log_control(e)
+        # stop the thread
+            return True
---
+++ demo.py
@@ -0,0 +1,163 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import re
+import sys
+import pickle
+import csv
+import _thread
+import re
+import random
+import os
+import time
+import hashlib
+from urllib.parse import urljoin
+import csv
+import threading
+import queue
+import tqdm
+from BaseClass import setup, PageInterface ,Controller ,WebsitePage
+
+class Demo(WebsitePage):
+    """
+    A class that represents a demo page of a website.
+
+    Attributes:
+    -----------
+    htm : str
+        The HTML content of the page.
+    url : str
+        The URL of the page.
+    tiitle : str
+        The title of the page.
+    dir : str
+        The directory where the page will be saved.
+    loggingfile : str
+        The path of the log file.
+
+    Methods:
+    --------
+    extract_text(dir)
+        Extracts the text from the page and saves it to a file.
+    extract_links(dir='')
+        Extracts all links from the page and saves them to a file.
+    worker_func(**kwargs)
+        A worker function for multithreading.
+    """
+class Demo(WebsitePage):
+    def __init__(self, htm, url, tiitle='' , dir='',loggingfile=''):
+        super().__init__(htm, url, tiitle, dir,loggingfile=loggingfile)
+        self.links = []
+        
+    def extract_text(self, dir):
+        '''
+        extract text from novel page
+        '''
+        ## extract all paragraphs from soup
+        try:
+            ### get all class you want and extract all detail
+            content = self.soup.find('div', class_='contentbox')
+            content = str(' '.join([i.get_text() for i in content]))
+            if len(content) == 0:
+                self.log_control(f'No content in page{self.title}')
+                return False
+            self.log_control(content[:25].strip() + '...'+content[-25:].strip()+'| '+str(len(content)))
+            self.extracted_pth = os.path.join(dir, str(self.title+'__'+self.uniqued_id) + '.txt').strip().replace(' ', '')
+            with open( self.extracted_pth, 'w') as f:
+                f.write(content)
+            self.extracted = True
+        except Exception as e:
+            self.log_control(e)
+            self.log_control('fail to extract text')
+            ## using base class extract text
+            super().extract_text(dir)
+
+    def extract_links(self, dir=''):
+        '''
+        extract all links from  page
+        reconstruct all links to absolute path
+        save if dir is not empty
+        return list of links(str)
+        '''
+        try:
+            links = []
+            for link in self.soup.find_all('a'):
+                #self.log_control(link)
+                href = link.get('href')
+                #self.log_control(href)
+                if href and not href.startswith('http'):
+                    href = urljoin(self.url, href)
+                #self.log_control(href)
+                links.append(href)
+            # remove None
+            links = [i for i in links if i]
+            # unique
+            links = list(set(links))
+            # remove all links not start with http 
+            links = [i for i in links if i.startswith('http')]
+            # remocelen > 86
+            if dir:
+                self.link_pth = os.path.join(dir, str(self.title+'__'+self.uniqued_id) + '.link').strip()
+                with open(self.link_pth, 'w') as f:
+                    for i in links:
+                        f.write(i+'\n')
+        except Exception as e:
+            self.log_control(e)
+            self.log_control('fail to extract links in NovelPage')
+            links = []
+        return links
+    def worker_func(self,**kwargs):
+        '''
+        worker function for multithreading
+        '''
+        self.extract_text(self.dir)
+        # extrac save html in **kwargs
+        if 'save_html' in kwargs:
+            if kwargs['save_html']:
+                self.save_html(self.dir)
+        link = self.extract_links()
+        state = self.get_state()
+        return link, state
+    
+
+
+def main():
+    # recorder driver init time
+
+    args = sys.argv
+    usearg=False
+    if len(args) == 3:
+        v1 = args[1]
+        v2 = args[2]
+        usearg=True
+    
+    
+    init_time = time.time()
+    driver = setup()
+
+    print('driver init time: ', time.time() - init_time)
+    init_url = 'YOUR_URL'
+    controller = Controller(init_url, '/PATH/to/save'
+                            ,thread_limit=3,driver=driver,page_class=NovelPage
+                            ,loggingfile='PATH/TO/LOGFILE')
+    time.sleep(1)
+    #assert False
+    init_time = time.time()
+
+    if usearg:
+        print('v1: ', v1)
+        print('v2: ', v2)
+        controller.loop(int(v1),int(v2),data={'save_html':False},wait_time=2,randomize=True)
+    else:
+        controller.loop(4,10,data={'save_html':True},wait_time=1,randomize=False)
+    print('loop time: ', time.time() - init_time)
+    toatl_size, total_len = controller.analy_dir()
+    print('total size: ', toatl_size)
+    print('total len: ', total_len)
+
+
+    driver.close()
+    driver.quit()
+print('start')           
+main()
+    
